cc553c04996f8238ca85b86ea50063dec03f5ea1
Adilet <muratov.adilet1@gmail.com>
2023-04-16 12:14:04 +0600

    first

--- dbeng/urls.py
+++ dbeng/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.StudentList.as_view(), name='student_list'),
     path('/about', views.AboutList.as_view(), name='about_list'),
+    path('/blogs', views.BlogsList.as_view(), name='blogs_list'),
+    path('/post', views.PostList.as_view(), name='post_list'),
+    path('/eng-test', views.EngTestList.as_view(), name='eng-test_list'),
+    path('/courses', views.CoursesList.as_view(), name='courses_list'),
+    path('/course-inner', views.CourseInnerList.as_view(), name='course-inner_list'),
+    path('/course-inner_individual', views.CourseInnerIndividualList.as_view(), name='course-inner_individual_list'),
+    path('/contact', views.ContactList.as_view(), name='contact_list'),
 ]
--- dbeng/views.py
+++ dbeng/views.py
@@ -15,3 +15,31 @@ class TeacherList(generic.ListView):
 class AboutList(generic.ListView):
     model = Teacher
     template_name = 'about_list.html'
+
+class BlogsList(generic.ListView):
+    model = Material
+    template_name = 'blogs_list.html'
+
+class PostList(generic.ListView):
+    model = Material
+    template_name = 'post_list.html'
+
+class EngTestList(generic.ListView):
+    model = Material
+    template_name = 'eng-test_list.html'
+
+class CoursesList(generic.ListView):
+    model = Course
+    template_name = 'courses_list.html'
+
+class CourseInnerList(generic.ListView):
+    model = Course
+    template_name = 'course-inner_list.html'
+
+class CourseInnerIndividualList(generic.ListView):
+    model = Course
+    template_name = 'course-inner_individual_list.html'
+
+class ContactList(generic.ListView):
+    model = Material
+    template_name = 'contact_list.html'
